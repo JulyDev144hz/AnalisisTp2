d7328615e0a1120c7ac08562dc6e18d4c345f2db
julydev144hz <ippolito.julianet36@gmail.com>
2022-07-11 08:29:32 -0300

    Funciona Ejs, Public y las fonts

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const express = require('express')
+const res = require('express/lib/response')
+const path = require('path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,'public/html/index.html'))
+})
+app.get('/inicio', (req, res)=>{
+    res.sendFile(path.join(__dirname,'public/html/inicio.html'))
+})
+
+app.get('/prueba',(req, res)=>{
+    res.render('index.ejs', {variable: "jose"})
+})
+
+
+app.use(express.static('public'))
+
+app.listen(3000,()=>{
+    console.log('Listen at port 3000')
+})

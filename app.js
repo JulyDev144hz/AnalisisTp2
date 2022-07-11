5fc48dc915ac90d56d5b4a5cf839e8ad9929355a
julydev144hz <ippolito.julianet36@gmail.com>
2022-07-11 14:11:47 -0300

    conectado y funcionando a la base de datos

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
+const mysql = require('mysql')
+
+class database{
+    constructor(){
+        this.connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'depositos'
+        })
+    }
+
+    connect(){
+        this.connection.connect((err)=>{
+            if (err) throw err;
+            console.log('Connected')
+        })
+    }
+
+    query(query){
+        return new Promise((resolve, reject)=>{
+            this.connection.query(query, (err, rows, fields)=>{
+                if (!err){
+                    resolve(rows);
+                }
+            })
+        })
+        
+    }
+
+}
+const db = new database()
+db.connect()
+//db.query('select * from deposito').then((datos)=> console.log(datos))
+
+
 const express = require('express')
 const body_parser= require('body-parser')
 const path = require('path')
-const { stringify } = require('querystring')
+
+
 
 const app = express()
 const htmldir = __dirname+'/public/html/'
@@ -18,12 +55,36 @@ app.get('/inicio', (req, res)=>{
 })
 
 app.get('/buscar',(req, res)=>{
-    res.sendFile(path.join(htmldir,'buscar.html'))
+    res.render('buscar.ejs', {title : "Buscar:"})
+})
+
+app.get('/ingresar',(req, res)=>{
+    res.render('buscar.ejs', {title : "Ingreso:"})
 })
 
 app.post('/buscar',(req, res)=>{
-    res.render('index.ejs', {req: stringify(req.body)})
-    console.log(req.body.Estante)
+    db.query(`select * from deposito where estante = ${req.body.estante} and fila = ${req.body.fila} and columna = ${req.body.columna} `).then((row)=>{
+        if (row[0]){
+            let contenido = row[0].producto
+            let cantidad = row[0].cantidad
+            res.render('pallets.ejs', {
+                pos: [req.body.estante, req.body.fila, req.body.columna],
+                contenido : contenido,
+                cantidad : cantidad,
+                vacio : true
+            })
+        }else{
+            res.render('pallets.ejs', {
+                pos: [req.body.estante, req.body.fila, req.body.columna],
+                vacio : false
+            })
+        }
+        
+    })
+
+
+    
+    //console.log(req.body)
 })
 
 

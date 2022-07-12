0986bbbe943bd396d2d24bd8c3de46eb087971e5
julydev144hz <ippolito.julianet36@gmail.com>
2022-07-12 11:50:37 -0300

    agregue el boton insertar, pero hay errores

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ class database{
             host: 'localhost',
             user: 'root',
             password: '',
-            database: 'depositos'
         })
     }
 
@@ -22,6 +21,8 @@ class database{
             this.connection.query(query, (err, rows, fields)=>{
                 if (!err){
                     resolve(rows);
+                }else{
+                    resolve(1);
                 }
             })
         })
@@ -32,7 +33,17 @@ class database{
 const db = new database()
 db.connect()
 //db.query('select * from deposito').then((datos)=> console.log(datos))
-
+db.query(`create database if not exists depositos`)
+db.query(`use depositos`)
+db.query(`
+    create table if not exists deposito(
+        estante int(2),
+        fila int(1),
+        columna int(1),
+        producto varchar(40),
+        cantidad int(8)
+    )`
+)
 
 const express = require('express')
 const body_parser= require('body-parser')
@@ -55,11 +66,29 @@ app.get('/inicio', (req, res)=>{
 })
 
 app.get('/buscar',(req, res)=>{
-    res.render('buscar.ejs', {title : "Buscar:"})
+    res.render('buscar.ejs', {title : "Buscar:", exito : 3})
 })
 
 app.get('/ingresar',(req, res)=>{
-    res.render('buscar.ejs', {title : "Ingreso:"})
+    res.render('buscar.ejs', {title : "Ingreso:", exito: 3})
+})
+
+
+app.post('/ingresar',(req, res)=>{
+    //console.log(`${req.body.estante},${req.body.fila}, ${req.body.columna},${req.body.producto},${req.body.cantidad}`)
+    //res.render('buscar.ejs',{title:"Ingreso:", exito: 2})
+    db.query(`insert into deposito(estante, fila, columna, producto, cantidad) values(${req.body.estante},${req.body.fila}, ${req.body.columna},"${req.body.producto}",${req.body.cantidad}) `).then((err)=>{
+        
+        if (err === 1){
+            res.render('buscar.ejs',{title:"Ingreso:", exito: 2})
+
+        }else{
+            res.render('buscar.ejs',{title:"Ingreso:", exito: 1})
+        }
+    })
+    
+        
+    
 })
 
 app.post('/buscar',(req, res)=>{
